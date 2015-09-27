862a485aebcfe2c671238ad0836d777420cbb79c
Brian Yang <brian@brianyang.me>
2015-09-27 07:21:06 -0400

    Fix socket URL

--- public/js/draw.js
+++ public/js/draw.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect('http://draw-anything.herokuapp.com:3000');
 socket.on('connect', function(data) {
     socket.emit('join', 'Hello World from client');
 });
