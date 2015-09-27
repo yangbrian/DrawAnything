1813d587a0a7ea7f4f74db4acd196cd4f200e41b
Kevin <kevin.li.3@stonybrook.edu>
2015-09-26 23:29:11 -0400

    added test case for nouns generated for guessing

--- public/js/index.js
+++ public/js/index.js
@@ -2,15 +2,29 @@
  * Created by Kevin on 9/26/2015.
  */
 var guesses = [];
+var words = ['dog', 'cat', 'pizza', 'donut', 'ice cream']
 
 $(document).ready(function() {
+
+    var randomWord = Math.random() * (words.length);
+    randomWord = parseInt(randomWord);
+    console.log(randomWord);
+    console.log(words);
+    $('#guessThis').append('<p>' + words[randomWord] + '<p>');
+
     $('#input').on('keyup', function(e) {
         var code = e.keyCode || e.which;
         if(code == 13) {
 
             guesses.push($('#input').val());
             console.log(guesses);
-            $('#listOfGuesses').append('<li>' + $('#input').val() + '</li>');
+
+            if($('#input').val().toLowerCase() === words[randomWord]) {
+                $('#correctGuess').append('<p>Correct! The answer was <b> ' + words[randomWord] + '</b>!</p>');
+            }
+            else {
+                $('#listOfGuesses').append('<li>' + $('#input').val() + '</li>');
+            }
 
             $('#input').val('');
         }
