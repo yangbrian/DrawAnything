82de487c22bc838b28a240442e3599f140c54a7c
Brian Yang <brian@brianyang.me>
2015-09-27 01:52:03 -0400

    Move word generation to server side

--- app.js
+++ app.js
@@ -59,19 +59,37 @@ app.use(function(err, req, res, next) {
   });
 });
 
+var words = ['time', 'issue', 'year', 'side', 'people', 'kind', 'way', 'head', 'day', 'house', 'man', 'service', 'thing', 'friend', 'woman', 'father', 'life', 'power', 'child', 'hour', 'world', 'game', 'school', 'line', 'state', 'end', 'family', 'member', 'student', 'law', 'group', 'car', 'country', 'city', 'problem', 'community', 'hand', 'name', 'part', 'president', 'place', 'team', 'case', 'minute', 'week', 'idea', 'company', 'kid', 'system', 'body', 'program', 'information', 'question', 'back', 'work', 'parent', 'government', 'face', 'number', 'others', 'night', 'level', 'monster', 'office', 'point', 'door', 'home', 'health', 'water', 'person', 'room', 'art', 'mother', 'war', 'area', 'history', 'money', 'party', 'story', 'result', 'fact', 'change', 'month', 'morning', 'lot', 'reason', 'right', 'research', 'study', 'girl', 'book', 'guy', 'eye', 'food', 'job', 'moment', 'word', 'air', 'business', 'teacher'];
+
+
+var currentWord = 'house';
+console.log('The current word is: ' + currentWord + '\n');
 
 // Socket
 io.on('connection', function(client) {
-  console.log('Client connected...');
+  //console.log('Client connected...');
 
   client.on('join', function(data) {
-    console.log(data);
+    //console.log(data);
     client.emit('messages', 'Hello from server');
   });
 
   client.on('guess', function(data) {
-    console.log('Client guessed ' + data);
-    client.broadcast.emit('guess', data);
+    console.log('User guessed ' + data + '\n');
+
+    if (data == currentWord) {
+      client.emit('guessResult', 'CORRECT');
+      client.broadcast.emit('guess', 'A user guessed correctly!');
+    } else {
+      client.emit('guessResult', 'INCORRECT');
+      client.broadcast.emit('guess', data);
+    }
+
+  });
+
+  client.on('newWord', function(data) {
+    currentWord = words[parseInt(Math.random() * (words.length))];
+    console.log('The new word is: ' + currentWord + '\n');
   });
 
 });
--- public/js/draw.js
+++ public/js/draw.js
@@ -8,16 +8,28 @@ socket.on('guess', function(data) {
     $('#listOfGuesses').append('<li>' + data + '</li>');
 });
 
+socket.on('guessResult', function(data) {
+    var value = $('#input').val();
+    if(data == 'CORRECT') {
+        $('#correctGuess').append('<p>Correct! The answer was <b> ' + value + '</b>!</p>');
+    }
+    else {
+        $('#listOfGuesses').append('<li>' + value + '</li>');
+    }
+
+    $('#input').val('');
+});
+
 var guesses = [];
-var words = ['time', 'issue', 'year', 'side', 'people', 'kind', 'way', 'head', 'day', 'house', 'man', 'service', 'thing', 'friend', 'woman', 'father', 'life', 'power', 'child', 'hour', 'world', 'game', 'school', 'line', 'state', 'end', 'family', 'member', 'student', 'law', 'group', 'car', 'country', 'city', 'problem', 'community', 'hand', 'name', 'part', 'president', 'place', 'team', 'case', 'minute', 'week', 'idea', 'company', 'kid', 'system', 'body', 'program', 'information', 'question', 'back', 'work', 'parent', 'government', 'face', 'number', 'others', 'night', 'level', 'monster', 'office', 'point', 'door', 'home', 'health', 'water', 'person', 'room', 'art', 'mother', 'war', 'area', 'history', 'money', 'party', 'story', 'result', 'fact', 'change', 'month', 'morning', 'lot', 'reason', 'right', 'research', 'study', 'girl', 'book', 'guy', 'eye', 'food', 'job', 'moment', 'word', 'air', 'business', 'teacher']
+
 
 $(document).ready(function() {
 
-    var randomWord = Math.random() * (words.length);
-    randomWord = parseInt(randomWord);
-    console.log(randomWord);
-    console.log(words);
-    $('#guessThis').append('<p>' + words[randomWord] + '<p>');
+    //var randomWord = Math.random() * (words.length);
+    //randomWord = parseInt(randomWord);
+    //console.log(randomWord);
+    //console.log(words);
+    //$('#guessThis').append('<p>' + words[randomWord] + '<p>');
 
     $('#input').on('keyup', function(e) {
         var code = e.keyCode || e.which;
@@ -28,15 +40,12 @@ $(document).ready(function() {
             guesses.push($('#input').val());
             console.log(guesses);
 
-            if($('#input').val().toLowerCase() === words[randomWord]) {
-                $('#correctGuess').append('<p>Correct! The answer was <b> ' + words[randomWord] + '</b>!</p>');
-            }
-            else {
-                $('#listOfGuesses').append('<li>' + $('#input').val() + '</li>');
-            }
 
-            $('#input').val('');
         }
     });
+
+    $('#newWord').on('click', function() {
+        socket.emit('newWord');
+    })
 });
 
